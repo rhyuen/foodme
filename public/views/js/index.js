793a77175b2f13252c27c40686aa34a22cd11a22
Robert Yuen <dungeonry@hotmail.com>
2016-08-08 18:50:02 -0700

    Yelp data shows up on the map again.

--- public/views/js/index.js
+++ public/views/js/index.js
@@ -16,9 +16,13 @@ if(navigator.geolocation){
   var map;
   navigator.geolocation.getCurrentPosition(function(position){
     $.post("/", {lat: position.coords.latitude, long: position.coords.longitude}, function(data){
+
       console.log(data);
 
-      var myLatLng = {lat:  position.coords.latitude, lng: position.coords.longitude};
+      var myLatLng = {
+        lat:position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
       map = new google.maps.Map(document.getElementById("map"), {
         center: myLatLng,
@@ -35,10 +39,12 @@ if(navigator.geolocation){
 
       }
 
-      for(var i = 0; i < data.length; i++){
+      var yelp = data.yelp_data;
+
+      yelp.map(function(yelp_listing){
         var otherMarkers = new google.maps.Marker({
-          position: {lat: data[i].gpsCoord.latitude, lng: data[i].gpsCoord.longitude},
-          title: data[i].name,
+          position: {lat: yelp_listing.gpsCoord.latitude, lng: yelp_listing.gpsCoord.longitude},
+          title: yelp_listing.name,
           icon: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"
         });
         otherMarkers.setMap(map);
@@ -46,22 +52,24 @@ if(navigator.geolocation){
         $("#restContent")
           .append($("<div/>", {class: "mdl-card mdl-shadow--2dp"})
           .append($("<div/>", {class: "mdl-card__title"})
-          .append($("<h2/>", {class: "mdl-card__title-text", text: data[i].name})))
-          .append($("<div/>", {class: "mdl-card__supporting-text", text: data[i].phone.substring(3)})
-          .append($("<p/>", {text: data[i].address[0]}))
-          .append($("<p/>", {text: "Dist: " + Math.ceil(data[i].relDistance) + "m"}))
-          .append($("<p/>", {text: "Score: " + data[i].cumulRating + "/5"})))
+          .append($("<h2/>", {class: "mdl-card__title-text", text: yelp_listing.name})))
+          .append($("<div/>", {class: "mdl-card__supporting-text", text: yelp_listing.phone.substring(3)})
+          .append($("<p/>", {text: yelp_listing.address[0]}))
+          .append($("<p/>", {text: "Dist: " + Math.ceil(yelp_listing.relDistance) + "m"}))
+          .append($("<p/>", {text: "Score: " + yelp_listing.cumulRating + "/5"})))
           .append($("<div/>", {class: "mdl-card__actions mdl-card--border"})
-          .append($("<a/>", {class: "mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect", text: data[i].categories[0][0]})))
+          .append($("<a/>", {class: "mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect", text: yelp_listing.categories[0][0]})))
           .append($("<div/>", {class: "mdl-card__menu"})
           .append($("<button/>", {class: "mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"})
           .append($("<i/>", {class: "material-icons", text: "places"})))));
-
-      }
+      });
     });
+
+    
   });
 }else{
   console.log("GPS Coords Permission Denied.");
-
-  $("#map").append($("<p/>", {text: "Refresh the page and say 'Yes' when prompted or the site provides no benefit."}));
+  $("#map").append($("<p/>", {
+    text: "Refresh the page and say 'Yes' when prompted or the site provides no benefit."
+  }));
 }
